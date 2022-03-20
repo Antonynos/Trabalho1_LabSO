c166b161beb9709131245a6880cec02d1fd182a8
Ulysses Ferreira <ulysses2.0@hotmail.com>
2022-03-20 15:53:37 -0300

    Update comandos.sh

--- comandos.sh
+++ comandos.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 usuario=$(whoami)
 info_disco=$(df -h | grep "C:" | awk '{print $2}')
-nome_processador=$(lscpu | grep "Model name:"| awk '{$1=$1}1' | cut -f 2 -d ":")
-memoria_ram=$(free -h | grep "Mem:" | cut -f 1 -d "6" | awk '{$1=$1}1' | cut -f 2 -d ":")
-n_thread=$(lscpu | grep "CPU(s):"| awk '{$1=$1}1' | cut -f 2 -d ":")
-n_nucleo=$(lscpu | grep "Core(s) per socket:"| awk '{$1=$1}1' | cut -f 2 -d ":")
+nome_processador=$(lscpu | grep "Model name:" | cut -f 2 -d ":" | awk '{$1=$1}1')
+memoria_ram=$(free -h | grep "Mem:" | awk '{print $2}')
+n_thread=$(lscpu | grep "CPU(s):"| cut -f 2 -d ":" | awk '{$1=$1}1')
+n_nucleo=$(lscpu | grep "Core(s) per socket:"| cut -f 2 -d ":" |  awk '{$1=$1}1')
 rede=$(ip addr | grep -E 'link/ether (.*?) brd' | tail -1 | awk '{ print $2}')
-echo "PC do $usuario tem um$nome_processador, memoria principal de $info_disco,$memoria_ram de ram,$n_thread threads,$n_nucleo nucleos, endereco de rede sendo $rede"
-lsusb | tr -d '\n'
+# echo "PC do $usuario tem um$nome_processador, memoria principal de $info_disco,$memoria_ram de ram,$n_thread threads,$n_nucleo nucleos, endereco de rede sendo $rede"
+# lsusb | tr -d '\n'
+
+echo -e " ______________________________________________________________________"
+echo -e " Nome do usuario | $usuario"
+echo -e " Processador     | $nome_processador"
+echo -e " Núcleos         | $n_nucleo"
+echo -e " Threads         | $n_thread"
+echo -e " Disco           | $info_disco"
+echo -e " Memoria RAM     | $memoria_ram"
+echo -e " ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾"
